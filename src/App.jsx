375c432fe30df195490457e7facd32d73d4f2a65
stuart-pike <110692951+stuart-pike@users.noreply.github.com>
2024-08-08 16:23:27 -0700

    Settings API to update data

--- src/App.jsx
+++ src/App.jsx
@@ -13,6 +13,7 @@ import Account from "./pages/Account";
 import Login from "./pages/Login";
 import PageNotFound from "./pages/PageNotFound";
 import AppLayout from "./ui/AppLayout";
+import Settings from "./pages/Settings";
 
 //setup cache
 const queryClient = new QueryClient({
@@ -38,6 +39,7 @@ function App() {
               <Route path="bookings" element={<Bookings />} />
               <Route path="cabins" element={<Cabins />} />
               <Route path="users" element={<Users />} />
+              <Route path="settings" element={<Settings />} />
               <Route path="account" element={<Account />} />
             </Route>
             <Route path="login" element={<Login />} />
